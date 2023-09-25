a860245141fcc2631ef44ae0088273393e876e3e
waykosabhi <102040576+waykosabhi@users.noreply.github.com>
2023-09-25 16:17:06 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,140 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/Project2.png";
+import projectImage2 from "./assets/projects/Project3.png";
+import projectImage3 from "./assets/projects/Project1.png";
+import projectImage4 from "./assets/projects/Project5.png";
+import projectImage5 from "./assets/projects/Project 4.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Abhishek Waykos",
+  tagline: "I'm Mern Stack Developer",
+  img: profile,
+  about: `As a Mern Stack developer, Web Design Applications using Micro services, React.js, Node.js. Experience in developing Web Applications using HTML, CSS and various web scripting technologies and web standards. Enthusiastic professional motivated by challenging projects and deadlines. Strong communicator with an
+   ability to convey ideas clearly with an emphasis on client satisfaction. I am looking for an opportunity to utilize my skills.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/abhishekwaykos/",
+  github: "https://github.com/waykosabhi",
+  twitter: "https://twitter.com/abhishek_waykos",
+  instagram: "https://www.instagram.com/mr_abhishek_pho2wala/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Skillhub IT Solution`,
+    Location: "Aurangabad",
+    Type: "Full Time",
+    Duration: "Jan 2023 - jun 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor in Computer Application",
+    Company: `Deogiri College `,
+    Location: "Aurangabad",
+    Type: "Full Time",
+    Duration: "Aug 2019 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Real Estate Templete",
+    image: projectImage1,
+    description: `This is sample project for Real Estate  Site.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://real-estate-templete.vercel.app/",
+    githubLink: "https://github.com/waykosabhi/Real_Estate_templete.git",
+  },
+  {
+    title: "MUI Templete",
+    image: projectImage2,
+    description: `This is sample project for Portfolio Site using Material UI.`,
+    techstack: "HTML/CSS, JavaScript, react, Material UI, Tailwind CSS",
+    previewLink: "https://react-mui-template-seven.vercel.app/",
+    githubLink: "https://github.com/waykosabhi/React_MUI_template.git",
+  },
+  {
+    title: "Water Jar Management",
+    image: projectImage4,
+    description: `This is sample project for Water Jar MAnagement system  Site.`,
+    techstack: "HTML/CSS, JavaScript, react, redux, node.js",
+    previewLink: "https://waykosabhi.github.io/Login_Registration_Page/",
+    githubLink: "https://github.com/waykosabhi/Login_Registration_Page.git",
+  },
+  {
+    title: "Login and  Registration Page",
+    image: projectImage4,
+    description: `This is sample project for Login and Registration page design .`,
+    techstack: "HTML/CSS, JavaScript,",
+    previewLink: "https://login-registration-page.vercel.app/",
+    githubLink: "https://github.com/waykosabhi/Water_Jar_Management-.git",
+  },
+  {
+    title: "Report Management",
+    image: projectImage5,
+    description: `This is sample project for Report Management in Hospital and Lab  .`,
+    techstack: "HTML/CSS, JavaScript, react, redux, node.js, express",
+    previewLink: "https://lab-report-management.vercel.app/",
+    githubLink: "https://github.com/waykosabhi/Lab_Report_Management-.git",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "waykosabhishek777@gmail.com",
+  phone: "+91 7261923815",
+};
